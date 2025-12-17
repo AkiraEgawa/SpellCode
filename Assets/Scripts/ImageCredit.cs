9aca59ecc5524adcc0d449160d011c4e92450bfe
Akira Egawa <lucian.ming.chen@gmail.com>
2025-12-16 19:20:49 -0800

    added Credits as a script too

---
+++ Assets/Scripts/ImageCredit.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+// This background is made by https://free-game-assets.itch.io/nature-landscapes-free-pixel-art
+
+public class ImageCredit : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
