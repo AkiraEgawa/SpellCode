23e57ea545cf348a78663b6332938168d47df965
Akira Egawa <lucian.ming.chen@gmail.com>
2025-12-22 19:04:09 -0800

    Made Spell Structure with math functions built in

---
+++ Assets/Scripts/SpellRuntime.cs
@@ -0,0 +1,149 @@
+using UnityEngine;
+using System;
+using System.Collections.Generic;
+
+// Object that controls the spell during runtime, essentialy the structure of it
+// Stack Convention:
+// First Pop = first argument (top of stack)
+public class SpellRuntime
+{
+    // Privates here
+    private Stack<object> stack = new Stack<object>();
+    private T Pop<T>()
+    {
+        if (stack.Count == 0){throw new System.InvalidOperationException("Stack Underflow");}
+        
+        return (T)stack.Pop();
+    }
+    
+    private void Push(object value)
+    {
+        stack.Push(value);
+    }
+
+    // Numbers Under, they add numbers, 1 through 9
+    // figure out the amount to add when reading it in by parsing.
+    public void addNum(object value)
+    {
+        stack.Push(value);
+    }
+
+    // Basic Math Operations Under, none should have returns (+-*/)
+    
+    public void Add()
+    {
+        float a = Pop<float>();
+        float b = Pop<float>();
+        Push(a+b);
+    }
+
+    public void Subtract()
+    {
+        float a = Pop<float>();
+        float b = Pop<float>();
+        Push(a-b);
+    }
+
+    public void Multiply()
+    {
+        float a = Pop<float>();
+        float b = Pop<float>();
+        Push(a*b);
+    }
+
+    public void Divide()
+    {
+        float a = Pop<float>();
+        float b = Pop<float>();
+
+        if (b == 0f){throw new InvalidOperationException("Division by zero");}
+
+        Push(a/b);
+    }
+
+    // Unary Operations (kinda useful)
+    public void Negate()
+    {
+        float a = Pop<float>();
+        Push(-a);
+    }
+
+    public void Abs()
+    {
+        float a = Pop<float>();
+        Push(MathF.Abs(a));
+    }
+
+    public void Sqrt()
+    {
+        float a = Pop<float>();
+
+        if (a < 0f){throw new InvalidOperationException("Sqrt of negative value");}
+
+        Push(MathF.Sqrt(a));
+    }
+
+    public void Ceil()
+    {
+        float a = Pop<float>();
+        Push(MathF.Ceiling(a));
+    }
+
+    public void Floor()
+    {
+        float a = Pop<float>();
+        Push(MathF.Floor(a));
+    }
+
+    // Power and Modulo
+
+    public void Power()
+    {
+        float a = Pop<float>();
+        float b = Pop<float>();
+        Push(MathF.Pow(a,b));
+    }
+
+    public void Mod()
+    {
+        float a = Pop<float>();
+        float b = Pop<float>();
+        Push(a % b);
+    }
+
+    // Trig
+
+    public void Sin()
+    {
+        float a = Pop<float>();
+        Push(Mathf.Sin(a));
+    }
+
+    public void Cos()
+    {
+        float a = Pop<float>();
+        Push(Mathf.Cos(a));
+    }
+
+    public void Tan()
+    {
+        float a = Pop<float>();
+        Push(Mathf.Tan(a));
+    }
+
+    // Min and Max
+    public void Min()
+    {
+        float a = Pop<float>();
+        float b = Pop<float>();
+        Push(Mathf.Min(a, b));
+    }
+
+    public void Max()
+    {
+        float a = Pop<float>();
+        float b = Pop<float>();
+        Push(Mathf.Max(a, b));
+    }
+
+}
